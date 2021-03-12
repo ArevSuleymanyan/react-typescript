7e871082726d2e5b6e1ccd996b716ecc37a5fd43
Arevik <kivera16@gmail.com>
2021-03-12 17:52:56 +0400

    change

--- frontend/src/components/Alert.tsx
+++ frontend/src/components/Alert.tsx
@@ -1,17 +1,15 @@
 import React from 'react'
 import { useAlert } from '../context/AlertContext'
 
-
-
-export const Alert: React.FC = (props:any) => {
-	const alert:any = useAlert()
+export const Alert: React.FC = (props: any) => {
+	const alert: any = useAlert()
 	if (!alert.visible) return null
 
 	return (
-		<div className='alert alert-info' onClick={alert.toggle}>
-			<p className='center'>
-				<strong>{props.message}</strong>
-			</p>
-		</div>
+			<div className='alert alert-info' onClick={alert.toggle}>
+				<p className='center'>
+					<strong>{props.message}</strong>
+				</p>
+			</div>
 	)
 }
