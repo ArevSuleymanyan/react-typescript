376ec92513097bc0f98c31edfdd25a31160c764d
Arevik <kivera16@gmail.com>
2021-03-15 18:23:22 +0400

    change

--- frontend/src/components/pages/HomePage.tsx
+++ frontend/src/components/pages/HomePage.tsx
@@ -6,10 +6,23 @@ export const HomePage: React.FC = () => {
 	if(user && user.id){
 		return (
 			<div>
-				{user.name}
+				<h1 className="guest">
+					Welcome home page {user.name} !!!
+				</h1>
+				<div id="background-wrap">
+					<div className="bubble x1"></div>
+					<div className="bubble x2"></div>
+					<div className="bubble x3"></div>
+					<div className="bubble x4"></div>
+					<div className="bubble x5"></div>
+					<div className="bubble x6"></div>
+					<div className="bubble x7"></div>
+					<div className="bubble x8"></div>
+					<div className="bubble x9"></div>
+					<div className="bubble x10"></div>
+				</div>
 			</div>
 		)
-
 	}
 	else {
 
