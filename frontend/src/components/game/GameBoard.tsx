6d45b4b899fe55bf3ac2ef0ef08c41eb44e4ca24
Arevik <kivera16@gmail.com>
2021-03-18 12:33:48 +0400

    typescript

--- frontend/src/components/game/GameBoard.tsx
+++ frontend/src/components/game/GameBoard.tsx
@@ -2,15 +2,14 @@ import React, { useContext, useEffect, useState } from 'react'
 import { GameContext } from '../../context/GameContext'
 import { GameCell } from './GameCell'
 import { TopPlayers } from './TopPlayers'
-import {GameScore} from './GameScore'
+import { GameScore } from './GameScore'
 import LinesLogic from '../../game/LinesLogic'
 
 const linesLogic = new LinesLogic()
 
 export const GameBoard: React.FunctionComponent = () => {
 	let i: number = 0
-	const { board } = useContext(GameContext);
-	console.log('board:', board)
+	const { board } = useContext(GameContext)
 	const [color, setColor] = useState<string>('')
 	const [first, setFirst] = useState(-1)
 	const [second, setSecond] = useState(-1)
@@ -19,7 +18,7 @@ export const GameBoard: React.FunctionComponent = () => {
 		linesLogic.runGame(board)
 	}, [])
 
-	const cellClick = (e) => {
+	const cellClick = (e: any) => {
 		let id = Number(e.target.id)
 		if (e.target.classList.length > 1) {
 			setFirst((prev) => (prev = id))
@@ -33,19 +32,16 @@ export const GameBoard: React.FunctionComponent = () => {
 		linesLogic.moveTheColor(first, second, color, board)
 	}
 
-	console.log({ first, second, color })
-
 	return (
 		<>
-			<div className="score-box">
+			<div className='score-box'>
 				<TopPlayers />
-				<GameScore/>
+				<GameScore />
 			</div>
-
 			<div className='board'>
-				{board.map((item: any) => {
+				{board.map((item: { color: string }) => {
 					i++
-					return <GameCell key={i} item={item} i={i} clickHandler={(e) => cellClick(e)} />
+					return <GameCell key={i} item={item} i={i} clickHandler={(e: any) => cellClick(e)} />
 				})}
 			</div>
 		</>
--- frontend/src/components/game/GameCell.tsx
+++ frontend/src/components/game/GameCell.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
+type Props = {
+        item: {
+                color:string
+        },
+        clickHandler: () => void,
+        i: number
+}
 
-export const GameCell: React.FC<{ item: any }> = ({ item, clickHandler, i }:any) => {
+export const GameCell: React.FC = ({ item, clickHandler, i }:Props) => {
 
  return (
          <div className={'cell ' + item.color} id={i-1} onClick={clickHandler}></div> 
