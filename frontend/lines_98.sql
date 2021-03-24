630ee371cc706d195d66989505aeb4386de1c88d
Arevik <kivera16@gmail.com>
2021-03-24 19:19:29 +0400

    change

---
+++ frontend/lines_98.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE color_lines;
+USE color_lines;
+
+CREATE TABLE users 
+(
+id int auto_increment not null primary key,
+name varchar(30) not null,
+email varchar(155) not null,
+password varchar(255) not null
+);
+
+CREATE TABLE game 
+(
+user_id int ,
+game_id int not null auto_increment primary key,
+game_board json not null,
+score int,
+foreign key (user_id) references users(id)
+);
+
+CREATE TABLE picture 
+(
+user_id int,
+img_id int not null auto_increment primary key,
+image varchar(255),
+foreign key (user_id) references user(id)
+);
